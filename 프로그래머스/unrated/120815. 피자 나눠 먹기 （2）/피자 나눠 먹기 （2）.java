b3e19c30f52e7fb88b00fe074f1822bad9b037a7
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-06 14:27:06 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int counter = 1;
+        while (true){
+            if (n * counter % 6 == 0) {
+                answer = n * counter / 6;
+                break;
+            }
+            counter++;
+        }
+        return answer;
+    }
+}
