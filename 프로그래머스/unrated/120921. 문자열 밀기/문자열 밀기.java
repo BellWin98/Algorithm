e5ac1fb5f3ff466f6c0d14d0bdacdb98cbc1ac27
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-08 12:23:31 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String A, String B) {
+        if (A.equals(B)){
+            return 0;
+        }
+        
+        StringBuilder sb = new StringBuilder(A);
+        int count = 0;
+        for (int i = 0; i < A.length(); i++){
+            char temp = sb.charAt(sb.length() - 1);
+            sb.delete(sb.length() - 1, sb.length());
+            sb.insert(0, temp);
+            count++;
+            if (sb.toString().equals(B)){
+                return count;
+            }
+        }
+        return -1;
+    }
+}
