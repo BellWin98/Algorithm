cd099575322bf934fe10b7564600f74d636b14ba
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-08 09:22:50 +0900

    [level unrated] Title: n 번째 원소까지, Time: 5.32 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -2,12 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> answerList = new ArrayList<>();
-        for (int i = 0; i < n; i++){
-            answerList.add(num_list[i]);
-        }
-        return answerList.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        // 내 풀이
+        // List<Integer> answerList = new ArrayList<>();
+        // for (int i = 0; i < n; i++){
+        //     answerList.add(num_list[i]);
+        // }
+        // return answerList.stream()
+        //     .mapToInt(Integer::intValue)
+        //     .toArray();
+        
+        // 다른 사람 풀이 1
+        // int[] answer = new int[n];
+        // for (int i = 0; i < answer.length; i++){
+        //     answer[i] = num_list[i];
+        // }
+        // return answer;
+        
+        // 다른 사람 풀이 2
+        return Arrays.copyOfRange(num_list, 0, n);
+        
     }
 }
