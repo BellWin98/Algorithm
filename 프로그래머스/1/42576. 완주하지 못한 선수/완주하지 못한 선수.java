01b977dd18f2147cf38ab3e82585fcef32b39078
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-12 21:05:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.97 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,7 +4,6 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> resultMap = new HashMap<>();
-        
         for (String s : participant){
             resultMap.put(s, resultMap.getOrDefault(s, 0) + 1);
         }
@@ -13,7 +12,9 @@ class Solution {
             // 완주자 명단에 있고, 동명이인이 없을 때
             if (resultMap.containsKey(s) && resultMap.get(s) == 1){
                 resultMap.remove(s);
-            } else if (resultMap.containsKey(s) && resultMap.get(s) != 1) {
+            } 
+            // 완주자 명단에 있고, 동명이인이 있을 때
+            else if (resultMap.containsKey(s) && resultMap.get(s) != 1) {
                 resultMap.put(s, resultMap.get(s) - 1);
             }
         }
@@ -25,3 +26,20 @@ class Solution {
         return answer;
     }
 }
+
+// 강사님 풀이
+// for (String member : completion){
+//     if (resultMap.get(member) > 1){
+//         resultMap.put(member, resultMap.get(member) - 1);
+//         continue;
+//     }
+//     if (resultMap.get(member) == 1){
+//         resultMap.remove(member);
+//     }
+// }
+
+// for (String member : resultMap.keySet()){
+//     answer = member;
+// }
+
+// return answer;
