afb6cd39fa84801d2f0eb15a0786d28beda77f90
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-12 14:59:33 +0900

    [level unrated] Title: A 강조하기, Time: 0.03 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,11 +1,14 @@
 class Solution {
     public String solution(String myString) {
-        myString = myString.replace('a', 'A');
-        for (int i = 0; i < myString.length(); i++){         
-            if (myString.charAt(i) != 'A'){
-                myString = myString.replace(myString.charAt(i), Character.toLowerCase(myString.charAt(i)));
-            }
-        }
-        return myString;
+        // 다른 사람 풀이
+        return myString.toLowerCase().replace('a', 'A');
     }
 }
+        // 내 풀이
+        // myString = myString.replace('a', 'A');
+        // for (int i = 0; i < myString.length(); i++){         
+        //     if (myString.charAt(i) != 'A'){
+        //         myString = myString.replace(myString.charAt(i), Character.toLowerCase(myString.charAt(i)));
+        //     }
+        // }
+        // return myString;
--- 프로그래머스/unrated/181874. A 강조하기/README.md
+++ 프로그래머스/unrated/181874. A 강조하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] A 강조하기 - 181874 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874?language=java) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 0.08 ms
+메모리: 69.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 14:57:47
+2023년 12월 2일 14:59:32
 
 ### 문제 설명
 
