0d3ba7e10a9a2fa6a3f3daba7f247d07083de4df
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-12 21:00:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.05 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 88.74 ms
+메모리: 96.3 MB, 시간: 82.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 17:48:27
+2023년 12월 2일 21:0:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,21 +4,22 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         Map<String, Integer> resultMap = new HashMap<>();
-        for (String member : participant){
-                resultMap.put(member, resultMap.getOrDefault(member, 0) + 1);
+        
+        for (String s : participant){
+            resultMap.put(s, resultMap.getOrDefault(s, 0) + 1);
         }
-        for (String member : completion){
-            if (resultMap.get(member) > 1){
-                resultMap.put(member, resultMap.get(member) - 1);
-                continue;
-            }
-            if (resultMap.get(member) == 1){
-                resultMap.remove(member);
+        
+        for (String s : completion) {
+            // 완주자 명단에 있고, 동명이인이 없을 때
+            if (resultMap.containsKey(s) && resultMap.get(s) == 1){
+                resultMap.remove(s);
+            } else if (resultMap.containsKey(s) && resultMap.get(s) != 1) {
+                resultMap.put(s, resultMap.get(s) - 1);
             }
         }
         
-        for (String member : resultMap.keySet()){
-            answer = member;
+        for (String s : resultMap.keySet()){
+            answer = s;
         }
         
         return answer;
