101c1d0e96e316c0943c0a6bedcbe40cc675cfd9
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-13 17:44:05 +0900

    [level 2] Title: 더 맵게, Time: 1358.96 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,19 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int count = 0;
+        // 강사님 풀이
+        int answer = 0;
         Queue<Integer> minHeap = new PriorityQueue<>();
         for (int number : scoville){
             minHeap.add(number);
         }
-        if (minHeap.peek() >= K){
-            return count;
+        while(true){
+            if (minHeap.peek() >= K){
+                break;
+            } else if (minHeap.size() == 1 && minHeap.peek() < K){
+                answer = -1;
+                break;
+            } else {
+                answer++;
+                int temp = minHeap.poll() + minHeap.poll() * 2;
+                minHeap.add(temp);
+            }
         }
-        while (minHeap.peek() < K && minHeap.size() > 1){
-            int value = minHeap.poll();
-            minHeap.add(value + (minHeap.poll() * 2));
-            count++;
-        }
-        return minHeap.peek() >= K ? count : -1; 
+        return answer;
     }
 }
+
+// 내 풀이
+//         int count = 0;
+//         Queue<Integer> minHeap = new PriorityQueue<>();
+//         for (int number : scoville){
+//             minHeap.add(number);
+//         }
+        // 첫 데이터가 K보다 크거나 같을 때 종료
+//         if (minHeap.peek() >= K){
+//             return count;
+//         }
+//         while (minHeap.peek() < K && minHeap.size() > 1){
+//             int value = minHeap.poll();
+//             minHeap.add(value + (minHeap.poll() * 2));
+//             count++;
+//         }
+        
+//         return minHeap.peek() >= K ? count : -1; 
