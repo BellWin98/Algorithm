e062a02f97fb7fb3f1e4c33c3b548d7ea7bf841f
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-14 17:16:11 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.25 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/README.md
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 숨어있는 숫자의 덧셈 (2) - 120864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864?language=java) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.35 ms
+메모리: 88.2 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 17:15:0
+2023년 12월 4일 17:16:9
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -4,9 +4,7 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         my_string = my_string.replaceAll("[a-zA-Z]", " ");
-        my_string = my_string.strip();
         String[] strArr = my_string.split(" ");
-        System.out.println(Arrays.toString(strArr));
         for (String str : strArr){
             if (!str.equals("")){
                 answer += Integer.parseInt(str);
