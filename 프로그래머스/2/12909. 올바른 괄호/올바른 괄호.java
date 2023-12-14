7efa6b4854864d7f9e88a9ed956238e3dea3b795
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-14 11:13:38 +0900

    [level 2] Title: 올바른 괄호, Time: 20.37 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,44 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        // String[] strArr = s.split("");
-        Stack<Character> strStack = new Stack<>();
+        // for문으로만 풀어보기
+        // 강사님 풀이
+        Deque<Character> myDeque = new ArrayDeque<>();
+        boolean answer = true;
         for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) == '('){
-                strStack.push(s.charAt(i));
+            if (myDeque.isEmpty()){
+                if (s.charAt(i) == ')'){
+                    answer = false;
+                    break;
+                }
+                myDeque.addLast(s.charAt(i));
             } else {
-                if (strStack.empty()){
-                    return false;
+                if (myDeque.peekLast() == '(' && s.charAt(i) == ')'){
+                    myDeque.pollLast();
                 } else {
-                    strStack.pop();
+                    myDeque.addLast(s.charAt(i));
                 }
             }
         }
-        return strStack.isEmpty() ? true : false; 
+        if (!myDeque.isEmpty()){
+            answer = false;
+        }
+        return answer;
     }
 }
+
+// 내 풀이
+// Stack<Character> strStack = new Stack<>();
+// for (int i = 0; i < s.length(); i++){
+//     if (s.charAt(i) == '('){
+//         strStack.push(s.charAt(i));
+//     } else {
+//         if (strStack.empty()){
+//             return false;
+//         } else {
+//             strStack.pop();
+//         }
+//     }
+// }
+// return strStack.isEmpty() ? true : false;
+
