7bc39c55f33205a925f05042f2fb7337f6d4a5a1
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-14 21:39:11 +0900

    [level unrated] Title: 이진수 더하기, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.java
@@ -2,35 +2,39 @@ import java.util.*;
 
 class Solution {
     public String solution(String bin1, String bin2) {
-        int decimalBin1 = 0;
-        int decimalBin2 = 0;
-        StringBuilder sb = new StringBuilder();
-        Stack<String> stack = new Stack<>();
-        int counter = 1;
-        for (int i = bin1.length() - 1; i >= 0; i--){
-            if (bin1.charAt(i) == '1'){
-                decimalBin1 += counter;
-            }
-            counter *= 2;
-        }
-        counter = 1;
-        for (int i = bin2.length() - 1; i >= 0; i--){
-            if (bin2.charAt(i) == '1'){
-                decimalBin2 += counter;
-            }
-            counter *= 2;
-        }
-        int decimalSum = decimalBin1 + decimalBin2;
-        if (decimalSum == 0){
-            return "0";
-        }
-        while (decimalSum != 0){
-            stack.push(String.valueOf(decimalSum % 2));
-            decimalSum /= 2;
-        }
-        while (!stack.isEmpty()){
-            sb.append(stack.pop());
-        }
-        return sb.toString();
+        // 다른 사람 풀이
+        return Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2), 2);
     }
 }
+
+// 내 풀이
+// int decimalBin1 = 0;
+// int decimalBin2 = 0;
+// StringBuilder sb = new StringBuilder();
+// Stack<String> stack = new Stack<>();
+// int counter = 1;
+// for (int i = bin1.length() - 1; i >= 0; i--){
+//     if (bin1.charAt(i) == '1'){
+//         decimalBin1 += counter;
+//     }
+//     counter *= 2;
+// }
+// counter = 1;
+// for (int i = bin2.length() - 1; i >= 0; i--){
+//     if (bin2.charAt(i) == '1'){
+//         decimalBin2 += counter;
+//     }
+//     counter *= 2;
+// }
+// int decimalSum = decimalBin1 + decimalBin2;
+// if (decimalSum == 0){
+//     return "0";
+// }
+// while (decimalSum != 0){
+//     stack.push(String.valueOf(decimalSum % 2));
+//     decimalSum /= 2;
+// }
+// while (!stack.isEmpty()){
+//     sb.append(stack.pop());
+// }
+// return sb.toString();
