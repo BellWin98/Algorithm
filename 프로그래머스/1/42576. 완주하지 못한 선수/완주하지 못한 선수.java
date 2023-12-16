034bb945d6049cb345374272b71b4c22e97c9a12
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-16 17:47:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.35 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,30 +2,52 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> participantMap = new HashMap<>();
         String answer = "";
-        Map<String, Integer> resultMap = new HashMap<>();
-        for (String s : participant){
-            resultMap.put(s, resultMap.getOrDefault(s, 0) + 1);
+        for (String person : participant){
+            participantMap.put(person, participantMap.getOrDefault(person, 0) + 1);
         }
         
-        for (String s : completion) {
-            // 완주자 명단에 있고, 동명이인이 없을 때
-            if (resultMap.containsKey(s) && resultMap.get(s) == 1){
-                resultMap.remove(s);
-            } 
-            // 완주자 명단에 있고, 동명이인이 있을 때
-            else if (resultMap.containsKey(s) && resultMap.get(s) != 1) {
-                resultMap.put(s, resultMap.get(s) - 1);
+        String answerKey = "";
+        for (String person : completion){
+            if (participantMap.containsKey(person)){
+                participantMap.put(person, participantMap.get(person) - 1);
             }
         }
         
-        for (String s : resultMap.keySet()){
-            answer = s;
+        for (String person : participantMap.keySet()){
+            if (participantMap.get(person) == 1){
+                answer = person;
+                break;
+            }
         }
-        
         return answer;
     }
 }
+
+// 내 풀이
+// String answer = "";
+// Map<String, Integer> resultMap = new HashMap<>();
+// for (String s : participant){
+//     resultMap.put(s, resultMap.getOrDefault(s, 0) + 1);
+// }
+
+// for (String s : completion) {
+//     // 완주자 명단에 있고, 동명이인이 없을 때
+//     if (resultMap.containsKey(s) && resultMap.get(s) == 1){
+//         resultMap.remove(s);
+//     } 
+//     // 완주자 명단에 있고, 동명이인이 있을 때
+//     else if (resultMap.containsKey(s) && resultMap.get(s) != 1) {
+//         resultMap.put(s, resultMap.get(s) - 1);
+//     }
+// }
+
+// for (String s : resultMap.keySet()){
+//     answer = s;
+// }
+
+// return answer;
 
 // 강사님 풀이
 // for (String member : completion){
