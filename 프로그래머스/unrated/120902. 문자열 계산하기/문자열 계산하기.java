5057cf9c9ff22db075eed9590646b59ad8326d20
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-18 15:46:18 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.37 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.java
@@ -2,26 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(String my_string) {
-        Deque<Integer> numberDeque = new ArrayDeque<>();
-        String[] splitArr = my_string.split("\\s");
-        String op = "";
-        for (String str : splitArr){
-            if (str.equals("+")){
-                op = "+";
-            } else if (str.equals("-")) {
-                op = "-";
-            } else {
-                numberDeque.addLast(Integer.parseInt(str));
-            }
-            
-            if (numberDeque.size() == 2){
-                if (op.equals("+")){
-                    numberDeque.addLast(numberDeque.pollFirst() + numberDeque.pollFirst());
-                } else {
-                    numberDeque.addLast(numberDeque.pollFirst() - numberDeque.pollFirst());
-                }
-            }
-        }
-        return numberDeque.pop();
+        Stack<Integer> numberStack = new Stack<>();
+        String[] splitArr = my_string.split("[\\s+]");
+        System.out.println(Arrays.toString(splitArr));
+        
+        return 0;
     }
 }
