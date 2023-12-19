8686bb55960053fcd20199aec3c61914af90ae24
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-19 15:50:11 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,28 +3,19 @@ import java.util.*;
 class Solution {
     static int max = 0;
     public int solution(int k, int[][] dungeons) {
-        List<List<int[]>> permutations = new ArrayList<>();
         boolean[] visited = new boolean[dungeons.length];
-        List<int[]> temp = new ArrayList<>();
-        permutation(visited, permutations, temp, dungeons, k, 0);
+        permutation(visited, dungeons, k, 0);
         return max;
     }
     
-    public void permutation(boolean[] visited, List<List<int[]>> permutations, 
-                            List<int[]> temp, int[][] dungeons, int k, int count){
+    public void permutation(boolean[] visited, int[][] dungeons, int k, int count){
         if (count > max){
             max = count;
         }
-        // if (count == dungeons.length){
-        //     permutations.add(new ArrayList<>(temp));
-        //     return;
-        // }
         for (int i = 0; i < dungeons.length; i++){
             if (!visited[i] && k >= dungeons[i][0]){
                 visited[i] = true;
-                temp.add(dungeons[i]);
-                permutation(visited, permutations, temp, dungeons, k - dungeons[i][1], count + 1);
-                temp.remove(temp.size() - 1);
+                permutation(visited, dungeons, k - dungeons[i][1], count + 1);
                 visited[i] = false;
             }
         }
