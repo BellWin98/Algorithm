daf58708a60794a9dda37e97395655e28b5aa76a
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-20 09:34:19 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     
     static int answer = 0;
+    static boolean checked = false;
     
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
@@ -14,11 +15,18 @@ class Solution {
         if (count > answer){
             answer = count;
         }
+        if (answer == dungeons.length){
+            checked = true;
+            return;
+        }
         for (int i = 0; i < dungeons.length; i++){
             if (!visited[i] && k >= dungeons[i][0]){
                 visited[i] = true;
                 permutation(visited, dungeons, k - dungeons[i][1], count + 1);
                 visited[i] = false;
+            }
+            if (checked == true){
+                break;
             }
         }
     }
