fda163d4fc41995f8ce72291e6d77ebfb0f6a217
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-21 22:48:47 +0900

    [level unrated] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.04 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.java
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(String myString, String pat) {
-        String answer = "";
-        Deque<Character> deque = new ArrayDeque<>();          
-        StringBuilder sbMyString = new StringBuilder();
-        StringBuilder sbPat = new StringBuilder();
-        String newMyString = sbMyString.append(myString).reverse().toString();
-        String newPat = sbPat.append(pat).reverse().toString();
-        int startIndex = newMyString.indexOf(newPat);
-        for (int i = startIndex; i < newMyString.length(); i++){
-            deque.addLast(newMyString.charAt(i));
-        }
-        while (!deque.isEmpty()){
-            answer += deque.pollLast() + "";
-        }
-        return answer;
+        // 2차 풀이
+        int index = myString.lastIndexOf(pat);
+        return myString.substring(0, index + pat.length());
     }
 }
+
+// 1차 풀이
+// Deque<Character> deque = new ArrayDeque<>();          
+// StringBuilder sbMyString = new StringBuilder();
+// StringBuilder sbPat = new StringBuilder();
+// String newMyString = sbMyString.append(myString).reverse().toString();
+// String newPat = sbPat.append(pat).reverse().toString();
+// int startIndex = newMyString.indexOf(newPat);
+// for (int i = startIndex; i < newMyString.length(); i++){
+//     deque.addLast(newMyString.charAt(i));
+// }
+// while (!deque.isEmpty()){
+//     answer += deque.pollLast() + "";
+// }
