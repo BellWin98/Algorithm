0eee4f6916daca942ba772f79b0432944dda0b2c
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-23 11:45:43 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.79 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 3.83 ms
+메모리: 78.4 MB, 시간: 3.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 11:41:21
+2023년 12월 6일 11:45:42
 
 ### 문제 설명
 
--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -6,14 +6,19 @@ class Solution {
         if (arr.length == 1){
             return new int[]{-1};
         }
-        List<Integer> numList = Arrays.stream(arr).boxed().collect(Collectors.toList());
-        int min = numList.get(0);
-        for (int i = 1; i < numList.size(); i++){
-            if (numList.get(i) < min){
-                min = numList.get(i);
-            }
-        }
-        numList.remove(Integer.valueOf(min));
-        return numList.stream().mapToInt(a -> a).toArray();
+        // Stream 풀이
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(a -> a != min).toArray();
     }
 }
+
+// 내 풀이
+// List<Integer> numList = Arrays.stream(arr).boxed().collect(Collectors.toList());
+// int min = numList.get(0);
+// for (int i = 1; i < numList.size(); i++){
+//     if (numList.get(i) < min){
+//         min = numList.get(i);
+//     }
+// }
+// numList.remove(Integer.valueOf(min));
+// return numList.stream().mapToInt(a -> a).toArray();
