4b203a9929b1ac00e6d837e690a0e26996589c5b
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-29 09:40:56 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,9 +2,12 @@ class Solution {
     static int answer = 0;
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
+        // dfs(0)으로 시작하면 안됨. 전체 노드를 순회해야 함
         for (int i = 0; i < n; i++){
             dfs(computers, visited, i);
         }
+        
+        // 전체노드 - 간선의 개수
         return n - answer;
     }
     
@@ -12,6 +15,7 @@ class Solution {
         visited[start] = true;
         for (int i = 0; i < computers[start].length; i++){
             if (!visited[i] && computers[start][i] == 1){
+                // 노드끼리 이어져있으면 간선의 개수 count
                 answer++;
                 dfs(computers, visited, i);
             }
