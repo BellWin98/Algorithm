e5a65dce3ea0c98f8b3a234b2a1d65a9f6c58af4
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2023-12-29 16:11:49 +0900

    [level 3] Title: 가장 먼 노드, Time: 34.59 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -4,7 +4,6 @@ class Solution {
     static boolean[] visited;
     static int[] distance;
     static List<List<Integer>> adjList;
-    
     public int solution(int n, int[][] edge) {
         visited = new boolean[n];
         distance = new int[n];
@@ -12,31 +11,33 @@ class Solution {
         for (int i = 0; i < n; i++){
             adjList.add(new ArrayList<>());
         }
-        for (int[] a : edge){
-            addEdge(a[0] - 1, a[1] - 1);
+        for (int[] arr : edge){
+            addEdge(arr[0] - 1, arr[1] - 1);
         }
+        
         bfs(0);
-        Arrays.sort(distance);
-        int max = distance[distance.length - 1];
-        int count = 0;
-        for (int number : distance){
-            if (number == max){
-                count++;
-            }
-        }
-        return count;
+        int max = Arrays.stream(distance).max().getAsInt();
+        int answer = (int) Arrays.stream(distance).filter(a -> a == max).count();
+        // Arrays.sort(distance);
+        // int max = distance[distance.length - 1];
+        // for (int number : distance){
+        //     if (number == max){
+        //         answer++;
+        //     }
+        // }
+        return answer;
     }
     
     void bfs(int start){
         Queue<Integer> queue = new LinkedList<>();
-        visited[start] = true;
         queue.add(start);
-        while (!queue.isEmpty()) {
-            int vertex = queue.poll(); // 실질적으로 방문
+        visited[start] = true;
+        while (!queue.isEmpty()){
+            int vertex = queue.poll();
             for (int target : adjList.get(vertex)){
                 if (!visited[target]){
                     distance[target] = distance[vertex] + 1;
-                    queue.add(target); // 방문 예정인 데이터를 큐에 담음
+                    queue.add(target);
                     visited[target] = true;
                 }
             }
@@ -45,6 +46,6 @@ class Solution {
     
     void addEdge(int start, int end){
         adjList.get(start).add(end);
-        adjList.get(end).add(start); // 단방향으로 주어졌을 때 사용
+        adjList.get(end).add(start);
     }
 }
