478da930abf237172a53f0c3b2a5bb089a459572
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-01-10 14:21:23 +0900

    [level 3] Title: 이중우선순위큐, Time: 1.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        
+        Queue<Integer> minPq = new PriorityQueue<>();
+        Queue<Integer> maxPq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (String op : operations){
+            String[] sp = op.split(" ");
+            if (sp[0].equals("I")){
+                minPq.add(Integer.parseInt(sp[1]));
+                maxPq.add(Integer.parseInt(sp[1]));
+            } else if (sp[0].equals("D")) {
+                if (!minPq.isEmpty() && sp[1].equals("-1")){
+                    int value = minPq.poll();
+                    maxPq.remove(value);
+                } else if (!maxPq.isEmpty() && sp[1].equals("1")){
+                    int value = maxPq.poll();
+                    minPq.remove(value);
+                }
+            }
+        }
+        if (minPq.isEmpty() && maxPq.isEmpty()){
+            return new int[]{0, 0};
+        } 
+        return new int[]{maxPq.poll(), minPq.poll()};
+    }
+}
