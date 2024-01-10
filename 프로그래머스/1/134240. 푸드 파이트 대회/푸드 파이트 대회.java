305c796604f5b5c414753c6e5ac2c1236562ee07
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-01-10 14:57:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.12 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addLast(0);
+        for (int i = food.length - 1; i > 0; i--){
+            int n = food[i] / 2;
+            while (n > 0){
+                deque.addLast(i);
+                n--;
+            }
+        }
+        for (int i = food.length - 1; i > 0; i--){
+            int n = food[i] / 2;
+            while (n > 0){
+                deque.addFirst(i);
+                n--;
+            }
+        }
+        while (!deque.isEmpty()){
+            sb.append(deque.pollFirst());
+        }
+        return sb.toString();
+    }
+}
