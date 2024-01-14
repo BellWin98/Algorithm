2f0deae6ec09307761734014f17e6fd8cb9df214
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-01-14 18:20:50 +0900

    [Bronze I] Title: 최소공배수, Time: 364 ms, Memory: 22288 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int max = Math.max(A, B);
+            int min = Math.min(A, B);
+            System.out.println(lcm(max, min));
+        }
+    }
+
+    static int gcd(int a, int b){
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b){
+        return a * b / gcd(a, b);
+    }
+}
