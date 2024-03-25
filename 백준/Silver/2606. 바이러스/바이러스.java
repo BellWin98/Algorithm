fca3cb2d89e9d243ef1d32c949397c446fe030fa
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-03-25 19:57:22 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static List<List<Integer>> graph = new ArrayList<>();
     static boolean[] visited;
-    static int count;
-    static List<List<Integer>> adjList;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int computers = sc.nextInt();
-        int edges = sc.nextInt();
-        count = 0;
-        visited = new boolean[computers + 1];
-        adjList = new ArrayList<>();
-        for (int i = 0; i <= computers; i++){
-            adjList.add(new ArrayList<>());
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int computers = Integer.parseInt(br.readLine());
+        int links = Integer.parseInt(br.readLine());
+
+        visited = new boolean[computers];
+
+        for (int i = 0; i < computers; i++){
+            graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < edges; i++){
-            addEdge(sc.nextInt(), sc.nextInt());
-        }
-        for (int i = 1; i <= computers; i++){
-            dfs(1);
+        for (int i = 0; i < links; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            addEdges(start, end);
         }
 
-        System.out.println(count);
+        dfs(0);
+
+        System.out.println(answer);
+    }
+
+    static void addEdges(int start, int end){
+        // 인덱스는 0부터 시작하므로 외부 리스트의 인덱스를 세팅하기 위해 start 변수에서 1씩 빼준다.
+        // end 변수는 인덱스 역할이 아닌 value 역할이므로, 그대로 넣어줌
+        graph.get(start - 1).add(end);
+        graph.get(end - 1).add(start);
     }
 
     static void dfs(int start){
+
         visited[start] = true;
-        for (int target : adjList.get(start)){
-            if (!visited[target]){
-                count++;
+        for (int i = 0; i < graph.get(start).size(); i++){
+            int target = graph.get(start).get(i) - 1;
+            if (!visited[target]) {
+                answer++;
                 dfs(target);
             }
         }
-    }
-
-    static void addEdge(int start, int end){
-        adjList.get(start).add(end);
-        adjList.get(end).add(start);
     }
 }
