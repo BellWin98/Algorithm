288d6e6b3285bb070d30c60c3cf77b5f2e0d4afd
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-03-25 16:11:47 +0900

    [Silver II] Title: DFS와 BFS, Time: 372 ms, Memory: 20888 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     static boolean[] visited;
-    static List<List<Integer>> adjList;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int V = sc.nextInt();
+    static List<List<Integer>> graph = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 정점의 개수
+        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
+        int V = Integer.parseInt(st.nextToken()); // 탐색 시작할 정점의 번호
 
-        visited = new boolean[N + 1];
-        adjList = new ArrayList<>();
+        visited = new boolean[N]; // 인덱스 0부터 시작
 
-        for (int i = 0; i <= N; i++){
-            adjList.add(new ArrayList<>());
+        for (int i = 0; i < N; i++){
+            graph.add(new ArrayList<>()); // 내부 리스트 객체 생성
         }
 
         for (int i = 0; i < M; i++){
-            addEdge(sc.nextInt(), sc.nextInt());
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            addEdge(start, end);
         }
 
-        for (int i = 1; i <= N; i++){
-            Collections.sort(adjList.get(i));
+        // 이중 리스트 내부의 각 리스트의 요소를 오름차순으로 정렬
+        for (List<Integer> index : graph) {
+            Collections.sort(index);
         }
 
-        dfs(V);
-        System.out.println();
+        dfs(V - 1);
+        // dfs에서 사용한 visited 배열 false로 초기화, bfs에서 써야하므로
         Arrays.fill(visited, false);
-        bfs(V);
-
+        System.out.println();
+        bfs(V - 1);
     }
 
     static void addEdge(int start, int end){
-        adjList.get(start).add(end);
-        adjList.get(end).add(start);
+        graph.get(start - 1).add(end);
+        graph.get(end - 1).add(start);
     }
 
     static void dfs(int start){
         visited[start] = true;
-        System.out.print(start + " ");
-        for (int node : adjList.get(start)){
-            if (!visited[node]){
-                dfs(node);
+        System.out.print(start + 1 + " ");
+        for (int i = 0; i < graph.get(start).size(); i++){
+            int target = graph.get(start).get(i) - 1;
+            if (!visited[target]){
+                dfs(target);
             }
         }
     }
 
     static void bfs(int start){
         Queue<Integer> Q = new LinkedList<>();
-        visited[start] = true;
         Q.offer(start);
-        while (!Q.isEmpty()){
+        visited[start] = true;
+        while(!Q.isEmpty()){
             int node = Q.poll();
-            System.out.print(node + " ");
-            for (int target : adjList.get(node)){
+            System.out.print(node + 1 + " ");
+            for (int i = 0; i < graph.get(node).size(); i++){
+                int target = graph.get(node).get(i) - 1;
                 if (!visited[target]){
-                    visited[target] = true;
                     Q.offer(target);
+                    visited[target] = true;
                 }
             }
         }
