8cb2b842fa322d20bc2a90c0513ee28b0d5fc3b3
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-05-08 17:14:47 +0900

    [Bronze V] Title: 개수 세기, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i = 0; i < N; i++){
+            if (Integer.parseInt(st.nextToken()) == v){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
