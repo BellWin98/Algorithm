1da1424438adac66706bc477d7959fc9403b5793
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-05-15 16:30:37 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int[] pieces = {1, 1, 2, 2, 2, 8};
+
+        for (int piece : pieces){
+            int whitePiece = Integer.parseInt(st.nextToken());
+            sb.append(piece - whitePiece).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
