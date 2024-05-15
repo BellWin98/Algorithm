b958eaea7bf40f5f8b9a54245658d5487634b211
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-05-15 21:16:53 +0900

    [Bronze I] Title: 단어 공부, Time: 384 ms, Memory: 38712 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,46 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<Character, Integer> info = new HashMap<>();
+        Map<Character, Integer> data = new HashMap<>();
+        String word = br.readLine();
+        word = word.toUpperCase();
 
-        // 대문자와 소문자를 구분하지 않으므로 대문자로 통일 (어차피 정답을 대문자로 출력해야 하니까)
-        String str = st.nextToken().toUpperCase();
-
-        // 길이가 1인 문자열은 비교할 필요 없으므로, 그대로 출력 후 return
-        if (str.length() == 1){
-            System.out.println(str);
+        // 한 글자일 경우 그냥 출력 후 종료
+        if (word.length() == 1){
+            System.out.println(word);
             return;
         }
 
-        // Map에 알파벳과 입력 횟수를 key-value로 저장
-        // getOrDefault 메서드로 반복되는 문자 count++
-        for (char c : str.toCharArray()){
-            info.put(c, info.getOrDefault(c, 0) + 1);
+        // map에 알파벳(key)과 등장 횟수(value) 삽입
+        for (char c : word.toCharArray()){
+            data.put(c, data.getOrDefault(c, 0) + 1);
         }
 
-        // Map의 key인 알파벳을 List로 변환
-        // 이유: Map 자료구조는 정렬이 불가하므로, key 값들을 List에 삽입 후
-        // map의 value를 기준으로 내림차순 정렬 (value가 큰 순서대로 정렬)
-        List<Character> alphabets = new ArrayList<>(info.keySet());
+        // map의 key값(Character)을 리스트에 삽입
+        List<Character> alphabetsSortedByCountDesc = new ArrayList<>(data.keySet());
 
-        // 리스트의 크기가 1일 경우, 첫번째 요소 출력 후 종료
-        if (alphabets.size() == 1){
-            System.out.println(alphabets.get(0));
+        // 리스트에 담긴 알파벳이 1개라면(동일한 알파벳이 여러 개 입력된 경우) 출력 후 종료
+        if (alphabetsSortedByCountDesc.size() == 1){
+            System.out.println(alphabetsSortedByCountDesc.get(0));
             return;
         }
-        alphabets.sort((o1, o2) -> info.get(o2).compareTo(info.get(o1)));
 
-        // List 1,2번째 key 값으로 map의 value를 비교했을 때 같으면 최빈값이 두 개 이상이므로 "?" 출력
-        if (info.get(alphabets.get(0)).equals(info.get(alphabets.get(1)))) {
+        // map의 알파벳 등장 횟수 기준으로 리스트 내림차순 정렬
+        alphabetsSortedByCountDesc.sort((o1, o2) -> data.get(o2) - data.get(o1));
+
+        // 리스트의 알파벳 최빈값이 2개 이상이면 ? 출력
+        if (data.get(alphabetsSortedByCountDesc.get(0))
+                .equals(data.get(alphabetsSortedByCountDesc.get(1)))){
             System.out.println("?");
         } else {
-            System.out.println(alphabets.get(0));
+            System.out.println(alphabetsSortedByCountDesc.get(0));
         }
     }
 }
