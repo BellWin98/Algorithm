75cc9b71c9bc16277ba4b203a43fa6bf938318bd
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2024-07-11 13:44:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    /**
+     * 1. 입력 문자열을 List에 넣는다.
+     * 2. ASCII 코드에서 소문자 a ~ z까지 뽑아서, List에 포함되는 지 확인. 이후 indexOf로 인덱스 추출.
+     * 3. list에 알파벳 없을 경우 -1 반환
+     * 4. 공백으로 구분해서 출력
+     */
     public static void main(String[] args) throws IOException {
-        char[] alphabets = {
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        StringBuilder sb = new StringBuilder();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine();
-
-        for (char alphabet : alphabets){
-            sb.append(word.indexOf(alphabet)).append(" ");
+        StringBuilder sb = new StringBuilder();
+        String inputStr = br.readLine();
+        List<Character> characterList = new ArrayList<>();
+        for (char c : inputStr.toCharArray()){
+            characterList.add(c);
         }
-
+        for (int i = 97; i <= 122; i++){
+            if (characterList.contains((char) i)) {
+                sb.append(characterList.indexOf((char) i)).append(" ");
+            } else {
+                sb.append(-1).append(" ");
+            }
+        }
         System.out.println(sb.toString().trim());
     }
 }
