7bd1afbd0938bd349a93a45ae4214958143f9100
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-09-11 14:22:38 +0900

    [level 2] Title: 소수 찾기, Time: 48.20 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 1.92 ms
+메모리: 90.4 MB, 시간: 48.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 6일 22:0:26
+2025년 09월 11일 14:22:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,64 +1,46 @@
 import java.util.*;
 
 class Solution {
-    static List<Integer> numberList;
-    static boolean[] visited;
-    static Set<List<Integer>> permutations; // 중복 순열 제거
-    static List<Integer> temp;
-    
     public int solution(String numbers) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        numberList = new ArrayList<>();
-        permutations = new HashSet<>();
-        temp = new ArrayList<>();
-        String[] numberSplit = numbers.split("");
-        for (String str : numberSplit){
-            numberList.add(Integer.parseInt(str));
-        }
-        visited = new boolean[numberList.size()];
-        permutation();
+        Set<Integer> numberSet = new HashSet<>();
+        boolean[] visited = new boolean[numbers.length()];
+        dfs(numbers, "", visited, numberSet);
         
-        for (List<Integer> permutation : permutations){
-            sb.setLength(0);
-            for (int number : permutation){
-                sb.append(number);
-            }
-            int number = Integer.parseInt(sb.toString());
-            if (number == 1){
-                continue;
-            }
-            // 소수 체크
-            if (isPrimeNumber(number)){
+        int answer = 0;
+        for (int number : numberSet) {
+            if (isPrime(number)) {
                 answer++;
             }
         }
+        
         return answer;
     }
-
-    void permutation(){
-        for (int i = 0; i < numberList.size(); i++){
-            if (!visited[i]){
+    
+    public static void dfs(String numbers, String current, boolean[] visited, Set<Integer> numberSet) {
+        if (!current.isEmpty()) {
+            numberSet.add(Integer.parseInt(current));
+        }
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!visited[i]) {
                 visited[i] = true;
-                temp.add(numberList.get(i));
-                permutation();
-                temp.remove(temp.size() - 1);
+                dfs(numbers, current + numbers.charAt(i), visited, numberSet);
                 visited[i] = false;
             }
         }
-        // temp가 공백이거나, 요소가 0으로 시작할 때 리스트 삽입 x
-        if (temp.size() == 0 || temp.get(0) == 0){
-            return;
-        }
-        permutations.add(new ArrayList<>(temp));
     }
     
-    boolean isPrimeNumber(int number){
-        for (int i = 2; i * i <= number; i++){
-            if (number % i == 0){
+    public static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
+        
         return true;
     }
 }
