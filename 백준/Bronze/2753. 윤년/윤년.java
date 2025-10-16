ebec839f974c2de1ef57eb9441f2b2aafdd660ae
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-16 10:04:29 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
-            System.out.println(1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
         } else {
             System.out.println(0);
         }
