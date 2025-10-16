d2cb9d50da4a8a24bc0ed5d08a4b79254464c568
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-16 11:20:11 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,18 +1,18 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Integer, Integer> hm = new HashMap<>();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(br.readLine());
-        int answer = 0;
-        for (int i = 0; i < N; i++){
-            if (Integer.parseInt(st.nextToken()) == v){
-                answer++;
-            }
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
-        System.out.println(answer);
+        System.out.println(hm.getOrDefault(Integer.parseInt(br.readLine()), 0));
     }
 }
