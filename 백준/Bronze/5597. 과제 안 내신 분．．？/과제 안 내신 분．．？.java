aedb8289253c82a21acc8c4a194a90e667829bf7
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-16 13:11:49 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 96 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,26 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> submitted = new ArrayList<>();
-
-        // 과제를 제출한 학생들 28명 담기
-        for (int i = 0; i < 28; i++){
-            submitted.add(Integer.parseInt(br.readLine()));
+        List<Integer> al = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            al.add(i);
         }
-
-        Collections.sort(submitted);
-
-        // 1 ~ 30번의 학생 중 submitted에 없는 학생 출력
-        for (int i = 1; i <= 30; i++){
-            if (!submitted.contains(i)) {
-                System.out.println(i);
-            }
+        for (int i = 0; i < 28; i++) {
+            int num = Integer.parseInt(br.readLine());
+            al.remove(Integer.valueOf(num));
         }
+        System.out.println(al.get(0));
+        System.out.println(al.get(1));
     }
 }
