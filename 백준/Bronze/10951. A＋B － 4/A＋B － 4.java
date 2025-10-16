9f93e4571a69286e9d64e5cdb4284478ce54f345
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-16 10:50:09 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,11 +1,17 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String str;
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
             System.out.println(A + B);
         }
     }
