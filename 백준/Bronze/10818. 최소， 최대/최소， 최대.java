3cdb65e402f36766000e906ae95dce1dd6ba5cb0
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-17 10:22:38 +0900

    [Bronze III] Title: 최소, 최대, Time: 1148 ms, Memory: 124472 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,27 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Integer> al = new ArrayList<>();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        while (st.hasMoreTokens()){
-            int number = Integer.parseInt(st.nextToken());
-            if (number > max){
-                max = number;
-            }
-            if (number < min){
-                min = number;
-            }
+        while (st.hasMoreTokens()) {
+            al.add(Integer.parseInt(st.nextToken()));
         }
-
-        System.out.println(min + " " + max);
+        Collections.sort(al);
+        bw.write(al.get(0) + " " + al.get(al.size() - 1));
+        bw.flush();
+        bw.close();
     }
 }
