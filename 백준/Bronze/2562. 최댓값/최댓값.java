7446c521061e1f93a56d156718e197380dabbda0
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-17 10:06:03 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 15988 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,26 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-
-    /**
-     * 1. 숫자 9개를 for문으로 입력받으면서 변수 max를 갱신한다.
-     * 2. 동시에, 순서 인덱스도 함께 갱신한다.
-     * 3. max와 index를 개행해서 출력한다.
-     */
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int max = Integer.MIN_VALUE;
-        int index = 0;
-        for (int i = 1; i <= 9; i++){
-            int number = Integer.parseInt(br.readLine());
-            if (number > max){
-                max = number;
-                index = i;
+        int maxIdx = 0;
+        for (int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > max) {
+                max = num;
+                maxIdx = i;
             }
         }
-        System.out.println(max + "\n" + index);
+        bw.write(max + "\n" + maxIdx);
+        bw.flush();
+        bw.close();
     }
 }
