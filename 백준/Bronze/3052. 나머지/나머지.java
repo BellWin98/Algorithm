8d2f4ece8433d4e1c03c76addf258e2ddbe194ec
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-18 15:40:58 +0900

    [Bronze II] Title: 나머지, Time: 116 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,27 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> numbers = new ArrayList<>();
-        int answer = 0;
-        numbers.add(-1); // 나머지 값 비교를 위한 초기화(음수) 값 삽입
-
-        for (int i = 0; i < 10; i++){
-            int number = Integer.parseInt(br.readLine());
-            number %= 42;
-
-            // 계산된 나머지가 리스트에 존재하지 않으면, 리스트에 나머지 삽입 후 answer 1 증가
-            if (!numbers.contains(number)) {
-                numbers.add(number);
-                answer++;
-            }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+            int key = num % 42;
+            hm.put(num % 42, hm.getOrDefault(key, 0) + 1);
         }
-
-        System.out.println(answer);
+        bw.write(hm.keySet().size() +"\n");
+        bw.flush();
+        bw.close();
     }
 }
