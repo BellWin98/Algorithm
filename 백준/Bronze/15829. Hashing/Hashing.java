80d3819b7492971a44f070f71a1984911141162c
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-23 10:12:40 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14496 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -6,12 +7,12 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int sum = 0;
+        BigInteger sum = BigInteger.ZERO;
         for (int i = 0; i < str.length(); i++) {
             int charNum = (int) str.charAt(i) - 96;
-            sum += charNum * (int) Math.pow(31, i);
+            sum = sum.add(BigInteger.valueOf(31).pow(i).multiply(BigInteger.valueOf(charNum)));
         }
-        bw.write(sum + "\n");
+        bw.write(sum.mod(BigInteger.valueOf(1234567891)) + "\n");
         bw.flush();
         bw.close();
     }
