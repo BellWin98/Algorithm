a83dfaf443fb66b4d183e74e91b955757779e387
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-24 10:38:52 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 16384 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,38 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] scores = new int[N];
-        int i = 0;
-        while (st.hasMoreTokens()) {
-            scores[i] = Integer.parseInt(st.nextToken());
-            i++;
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(solution(scores));
-    }
-
-    static double solution(int[] scores) {
-        int M = Integer.MIN_VALUE;
-        double sumOfScores = 0.0;
-        for (int score : scores) {
-            if (score > M) {
-                M = score;
-            }
+        Arrays.sort(arr);
+        int M = arr[arr.length - 1];
+        double sum = 0;
+        for (int score : arr) {
+            sum += (double) score / M * 100;
         }
-        for (int score : scores) {
-            sumOfScores += getNewScore(M, score);
-        }
-
-        return sumOfScores / scores.length;
-    }
-
-    static double getNewScore(int M, int score) {
-        return (double) score / M * 100;
+        bw.write(sum / N + "\n");
+        bw.flush();
+        bw.close();
     }
 }
