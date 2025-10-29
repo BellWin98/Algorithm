3a7662cd2fa9d738c7ea75b16827a571fb901a5c
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-29 09:44:27 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 688 ms, Memory: 53608 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] plane = new int[N][2];
+        for (int i = 0; i < plane.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            plane[i][0] = Integer.parseInt(st.nextToken());
+            plane[i][1] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+        Arrays.sort(plane, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+        for (int[] arr : plane) {
+            bw.write(arr[0] + " " + arr[1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
