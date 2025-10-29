bac311b3abffe4fdddc61d510d60cc139e3d6815
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-29 10:41:45 +0900

    [Silver IV] Title: 수 찾기, Time: 540 ms, Memory: 44236 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -18,7 +18,7 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         br.close();
         while (st.hasMoreTokens()) {
-            if (binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+            if (Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
                 bw.write("1\n");
             } else {
                 bw.write("0\n");
@@ -26,22 +26,5 @@ public class Main {
         }
         bw.flush();
         bw.close();
-    }
-
-    public static int binarySearch(int[] arr, int key) {
-        int low = 0;
-        int high = arr.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (key > arr[mid]) {
-                low = mid + 1;
-            } else if (key < arr[mid]) {
-                high = mid - 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
     }
 }
