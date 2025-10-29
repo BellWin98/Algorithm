480b53a89594e6af2bb1cf302c8006a64438efe9
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-29 10:40:44 +0900

    [Silver IV] Title: 수 찾기, Time: 560 ms, Memory: 42540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,20 +7,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        Map<String, Integer> hm = new HashMap<>();
+        int[] arr = new int[N];
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            hm.put(st.nextToken(), 1);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         br.close();
         while (st.hasMoreTokens()) {
-            int answer = hm.getOrDefault(st.nextToken(), 0) > 0 ? 1 : 0;
-            bw.write(answer + "\n");
+            if (binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+                bw.write("1\n");
+            } else {
+                bw.write("0\n");
+            }
         }
         bw.flush();
         bw.close();
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (key > arr[mid]) {
+                low = mid + 1;
+            } else if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
