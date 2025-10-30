e97fd7c24431c492feb354ec19caf5cdb0b74efe
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-10-30 16:17:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,34 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
+        br.close();
         int answer = 0;
-
-        // N이 3보다 작으면 -1 return
-        if (N < 3){
-            System.out.println(-1);
-            return;
+        if (N % 5 == 0) {
+            answer = N / 5;
+            N = 0;
         }
-
-        while(N > 0){
-            // N이 5의 배수이면 나눈 몫을 answer에 더해주고 answer 출력 후 종료
-            if (N % 5 == 0){
-                answer += N / 5;
-                System.out.println(answer);
-                return;
-            }
+        while (N >= 3) {
             N -= 3;
             answer++;
+            if (N % 5 == 0) {
+                answer += N / 5;
+                N = 0;
+            }
         }
-
-        if (N == 0){
-            System.out.println(answer);
-        } else {
-            System.out.println(-1);
+        if (N > 0) {
+            answer = -1;
         }
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
     }
 }
