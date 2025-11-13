854427b0fe0d087241d70050adf9ecc4987f730f
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2025-11-13 15:08:03 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -7,22 +7,19 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
         List<Integer> al = new ArrayList<>();
         while (st.hasMoreTokens()) {
             al.add(Integer.parseInt(st.nextToken()));
         }
         Collections.sort(al);
-        List<Integer> sumAl = new ArrayList<>();
         int sum = 0;
+        int prev = 0;
         for (int number : al) {
-            sum += number;
-            sumAl.add(sum);
+            prev += number;
+            sum += prev;
         }
-        int answer = 0;
-        for (int number : sumAl) {
-            answer += number;
-        }
-        bw.write(answer + "\n");
+        bw.write(sum + "\n");
         bw.flush();
         bw.close();
     }
