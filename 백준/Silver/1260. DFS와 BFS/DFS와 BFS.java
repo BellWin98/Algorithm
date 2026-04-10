985560b47c88ee40c7c2f840784217a41448a07c
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2026-04-10 21:33:55 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 23256 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,76 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static List<List<Integer>> adjList = new ArrayList<>();
+    static int[][] graph;
     static boolean[] visited;
-
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken()) - 1;
+        int V = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N];
+        graph = new int[N + 1][N + 1];
+        visited = new boolean[N + 1];
 
-        for (int i = 0; i < N; i++) {
-            adjList.add(new ArrayList<>());
-        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken()) - 1;
-            int end = Integer.parseInt(st.nextToken()) - 1;
-            addEdge(start, end);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u][v] = 1;
+            graph[v][u] = 1;
         }
 
-        // 연결되어 있는 자식 노드를 오름차순으로 정렬 (작은 번호부터 방문)
-        for (List<Integer> children : adjList) {
-            children.sort(Comparator.naturalOrder());
-        }
-
+        br.close();
         dfs(V);
+        System.out.println(sb.toString().trim());
+        sb.setLength(0);
         Arrays.fill(visited, false);
-        System.out.println();
         bfs(V);
+        System.out.println(sb.toString().trim());
     }
 
-    public static void addEdge(int start, int end) {
-        adjList.get(start).add(end);
-        adjList.get(end).add(start);
-    }
-
-    public static void dfs(int root) {
-        visited[root] = true;
-        System.out.print(root + 1);
-        for (int child : adjList.get(root)) {
-            if (!visited[child]) {
-                System.out.print(" ");
-                dfs(child);
+    public static void dfs(int node) {
+        visited[node] = true;
+        sb.append(node).append(" ");
+        for (int i = 1; i < graph.length; i++) {
+            if (graph[node][i] == 1 && !visited[i]) {
+                dfs(i);
             }
         }
     }
 
-    public static void bfs(int root) {
+    public static void bfs(int node) {
         Queue<Integer> Q = new LinkedList<>();
-        Q.add(root);
-        visited[root] = true;
-
+        Q.add(node);
+        visited[node] = true;
         while (!Q.isEmpty()) {
-            int index = Q.poll();
-            System.out.print(index + 1);
-            for (int child : adjList.get(index)) {
-                if (!visited[child]) {
-                    Q.add(child);
-                    visited[child] = true;
+            int target = Q.poll();
+            sb.append(target).append(" ");
+            for (int i = 1; i < graph.length; i++) {
+                if (graph[target][i] == 1 && !visited[i]) {
+                    Q.add(i);
+                    visited[i] = true;
                 }
             }
-            System.out.print(" ");
         }
+
+
     }
 }
