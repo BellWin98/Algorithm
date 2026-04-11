bcd44f59659b8f25108cc13fb53f7bc2f67c9711
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2026-04-11 16:39:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 15940 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,65 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
+    static int[][] graph;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
+    static int[] dx = {-1 ,1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] map;
-    static int N;
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int groups = 0;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-
-        map = new int[N][N];
+        graph = new int[N][N];
         visited = new boolean[N][N];
-        List<Integer> houses = new ArrayList<>();
 
-        for (int i = 0; i < N; i++) {
-            char[] chars = br.readLine().toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                map[i][j] = Character.getNumericValue(chars[j]);
+        for (int i = 0; i < graph.length; i++) {
+            char[] line = br.readLine().toCharArray();
+            for (int j = 0; j < line.length; j++) {
+                graph[i][j] = Character.getNumericValue(line[j]);
             }
         }
 
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length; j++) {
-                if (!visited[i][j] && map[i][j] == 1) {
-                    houses.add(dfs(j, i));
+        br.close();
+
+        int groups = 0;
+        List<Integer> countsPerGroup = new ArrayList<>();
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph.length; j++) {
+                if (graph[i][j] == 1 && !visited[i][j]) {
+                    dfs(i, j);
+                    countsPerGroup.add(count);
+                    count = 0;
                     groups++;
                 }
             }
         }
-
-        Collections.sort(houses);
-        System.out.println(groups);
-        for (int house : houses) {
-            System.out.println(house);
+        Collections.sort(countsPerGroup);
+        bw.write(groups + "\n");
+        for (int count : countsPerGroup) {
+            bw.write(count + "\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 
-    public static int dfs(int x, int y) {
-
-        visited[y][x] = true;
-        int houseCount = 1;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[ny][nx] && map[ny][nx] == 1) {
-                houseCount += dfs(nx, ny);
+    public static void dfs(int i, int j) {
+        visited[i][j] = true;
+        count++;
+        for (int k = 0; k < 4; k++) {
+            int ny = i + dy[k];
+            int nx = j + dx[k];
+            if (ny >= 0 && nx >= 0 && ny < graph.length && nx < graph.length &&
+                    graph[ny][nx] == 1 && !visited[ny][nx]) {
+                dfs(ny, nx);
             }
         }
-
-        return houseCount;
     }
 }
