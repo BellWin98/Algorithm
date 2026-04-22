96ab241685c73f1d1b87699355f2c2af6d4d2296
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2026-04-22 21:15:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.10 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 52.4 MB, 시간: 4.53 ms
+메모리: 55 MB, 시간: 7.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 20:19:13
+2026년 04월 22일 21:15:32
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visited;
-    static int[][] distance;
-    static int[][] moves;
-    static int m, n;
     
-    static class Node{
-        int x, y;
-        public Node(int x, int y){
+    static class Node {
+        int x;
+        int y;
+        int distance;
+
+        Node(int x, int y, int distance) {
             this.x = x;
             this.y = y;
+            this.distance = distance;
         }
     }
     
     public int solution(int[][] maps) {
-        m = maps.length;
-        n = maps[0].length;
-        visited = new boolean[m][n];
-        distance = new int[m][n];
-        moves = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int i = 0; i < m; i++){
-            Arrays.fill(distance[i], 1);
-        }
-        Node startNode = new Node(0, 0);
-        Node endNode = new Node(m - 1, n - 1);
-        int shortest = bfs(startNode, endNode, maps);
-        
-        return shortest == -1 ?  -1 : shortest;
-    }
-    
-    int bfs(Node start, Node destination, int[][] maps){
-        visited[start.x][start.y] = true;
+        int m = maps.length;
+        int n = maps[0].length;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        boolean[][] visited = new boolean[m][n];
         Queue<Node> Q = new LinkedList<>();
-        Q.add(start);
-        while(!Q.isEmpty()){
-            Node node = Q.poll();
-            for (int[] move : moves){
-                int nx = node.x + move[0];
-                int ny = node.y + move[1];
-                if (nx == destination.x && ny == destination.y){
-                    return distance[node.x][node.y] + 1;
-                }
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && maps[nx][ny] == 1){
-                    visited[nx][ny] = true;
-                    distance[nx][ny] = distance[node.x][node.y] + 1;
-                    Q.add(new Node(nx, ny));
+        Q.add(new Node(0, 0, 1));
+        visited[0][0] = true;
+
+        while (!Q.isEmpty()) {
+            Node cur = Q.poll();
+            if (cur.x == n - 1 && cur.y == m - 1) {
+                return cur.distance;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[ny][nx] == 1 && !visited[ny][nx]) {
+                    Q.add(new Node(nx, ny, cur.distance + 1));
+                    visited[ny][nx] = true;
                 }
             }
         }
