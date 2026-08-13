89a7addf3d6391d13ec5a7772bb4faa1dc91088f
JongSeungHan <60949121+BellWin98@users.noreply.github.com>
2026-08-13 14:24:01 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,11 +1,3 @@
 def solution(array):
-    answer = []
     max_num = max(array)
-    answer.append(max_num)
-    
-    for i in range(0, len(array)):
-        if array[i] == max_num:
-            answer.append(i)
-            break
-    
-    return answer
+    return [max_num, array.index(max_num)]
